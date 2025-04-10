004b2c31fe6541cf51c07c5122220ed6377641b2
MUSHUBO <shubo3132@gmail.com>
2025-04-10 12:43:45 +0600

    the website has been made fully responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='w-full h-screen relative overflow-hidden mb-14'>
+        <div className='relative w-full lg:h-screen overflow-hidden mb-14'>
             <div className=''>
                 <img className='w-full h-full bg-cover bg-center' src="https://i.ibb.co.com/8nLMp66G/Banner-min.jpg" alt="" />
             </div>
-            <div className='absolute inset-0 text-white flex flex-col items-start justify-center ml-32 gap-5'>
-                <h1 className='text-5xl font-semibold'>Bid on Unique Items from <br/> Around the World</h1>
+            <div className='absolute inset-0 text-white flex flex-col items-start justify-center lg:ml-32 ml-7 lg:gap-8 gap-4 '>
+                <h1 className='lg:text-5xl text-3xl font-semibold'>Bid on Unique Items from <br/> Around the World</h1>
                 <p className='font-light text-xl'>Discover rare collectibles, luxury goods, and vintage treasures <br/> in our curated auctions</p>
                 <button className='btn rounded-3xl'>Explore Auctions</button>
             </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='shadow-sm'>
-            <div className="navbar bg-base-100 w-10/12 mx-auto flex justify-between">
+        <div className='shadow-sm navbar'>
+            <div className=" bg-base-100 w-10/12 mx-auto flex justify-between">
                 <div className="">
                     <a className="text-2xl text-[#003EA4] ">Auction<span className='text-[#FFD337] font-bold'>Gallery</span></a>
                 </div>
 
-                <ul className='flex gap-16 font-medium'>
-                    <li className='hover:bg-gray-200 p-2 rounded-md'><a href="">Home</a></li>
-                    <li className='hover:bg-gray-200 p-2 rounded-md'><a href="">Auctions</a></li>
-                    <li className='hover:bg-gray-200 p-2 rounded-md'><a href="">Categories</a></li>
-                    <li className='hover:bg-gray-200 p-2 rounded-md'><a href="">How to works</a></li>
-                </ul>
+                <div className='hidden lg:inline'>
+                    <ul className='flex gap-16 font-medium'>
+                        <li className='hover:bg-gray-200 p-2 rounded-md'><a href="">Home</a></li>
+                        <li className='hover:bg-gray-200 p-2 rounded-md'><a href="">Auctions</a></li>
+                        <li className='hover:bg-gray-200 p-2 rounded-md'><a href="">Categories</a></li>
+                        <li className='hover:bg-gray-200 p-2 rounded-md'><a href="">How to works</a></li>
+                    </ul>
+                </div>
 
                 <div className="flex items-center gap-4">
 
-                    <div className="navbar-end">
+                    <div className="">
                         <button className="btn btn-ghost btn-circle">
                             <div className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
