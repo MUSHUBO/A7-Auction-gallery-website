ed2b4c3deda7344c3aa4560dd6e8c7b8b675511b
MUSHUBO <shubo3132@gmail.com>
2025-04-10 08:35:48 +0600

    added the banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-4xl text-sky-500'>hello world</h1>
+      <Navbar></Navbar>
 
       
     </>
